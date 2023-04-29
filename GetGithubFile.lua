f0b374603b9940f9ca19ad444c983387cebfb2d5
DeividComSono <116926351+DeividComSono@users.noreply.github.com>
2023-04-29 20:54:32 -0300

    Update GetGithubFile.lua

--- GetGithubFile.lua
+++ GetGithubFile.lua
@@ -3,7 +3,7 @@ local Request = (syn and syn.request) or request or http_request or HttpPost
 
 function thing:getResponse(args)
     local response = Request({
-        Url = string.format("https://raw.githubusercontent.com/%s/%s/main/%s", args.Owner, args.Repository:gsub(" ","%20"), args.File:gsub(" ","%20")),
+        Url = string.format("https://raw.githubusercontent.com/%s/%s/main/%s", args.Owner, args.Repository:gsub(" ","%%20"), args.File:gsub(" ","%%20")),
         Method = "GET",
         Headers = {
             ["Authorization"] = args.Token and "token " .. tostring(args.Token) or "",
